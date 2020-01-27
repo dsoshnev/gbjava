c2592a1395590b14e4112f4a427795f3820aa2b1
dsoshnev <dsoshnev@mail.ru>
2020-01-27 19:06:04 +0300

    Выполнил задание 4.2,4.3 урока 4

--- GBProject/src/lesson4/App4.java
+++ GBProject/src/lesson4/App4.java
@@ -4,8 +4,8 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App4 {
-    public static final int GAME_SIZE = 3;
-
+    public static final int GAME_SIZE = 5;
+    public static final int COUNT_TO_WIN = 4;
     public static final char CELL_EMPTY = '*';
     public static final char CELL_X = 'X';
     public static final char CELL_O = 'O';
@@ -33,7 +33,7 @@ public class App4 {
         do {
             humanMove(CELL_X);
             printGameField();
-            if (isWin(CELL_X)) {
+            if (isWin(CELL_X, COUNT_TO_WIN)) {
                 System.out.println("Вы победили!");
                 break;
             }
@@ -43,7 +43,7 @@ public class App4 {
 
             botMove(CELL_O);
             printGameField();
-            if (isWin(CELL_O)) {
+            if (isWin(CELL_O, COUNT_TO_WIN)) {
                 System.out.println("Вы проиграли!");
                 break;
             }
@@ -112,9 +112,10 @@ public class App4 {
         return true;
     }
 
-    private static boolean isWin(char c) {
+    private static boolean isWin(char c, int countToWin) {
 
-        if(gameField[0][0] == c && gameField[0][1] == c && gameField[0][2] == c ) return true;
+        //1. Полностью разобраться с кодом, попробовать переписать с нуля, стараясь не подглядывать в методичку;
+        /*if(gameField[0][0] == c && gameField[0][1] == c && gameField[0][2] == c ) return true;
         if(gameField[1][0] == c && gameField[1][1] == c && gameField[1][2] == c ) return true;
         if(gameField[2][0] == c && gameField[2][1] == c && gameField[2][2] == c ) return true;
 
@@ -124,7 +125,57 @@ public class App4 {
 
         if(gameField[0][0] == c && gameField[1][1] == c && gameField[2][2] == c ) return true;
         if(gameField[0][2] == c && gameField[1][1] == c && gameField[2][0] == c ) return true;
+        */
 
+
+        //2. Переделать проверку победы, чтобы она не была реализована просто набором условий, например,
+        // с использованием циклов.
+        //3. * Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и количества фишек 4.
+        // Очень желательно не делать это просто набором условий для каждой из возможных ситуаций;
+        //Проверка строк на победу
+        for (int i = 0; i < GAME_SIZE; i++) {
+            int count = 0;
+            for (int j = 0; j < GAME_SIZE; j++) {
+                if(gameField[i][j] == c) {
+                    count++;
+                    if (count == countToWin) return true;
+                } else {
+                    count = 0;
+                }
+            }
+        }
+        //Проверка столбцов на победу
+        for (int  j = 0; j < GAME_SIZE; j++) {
+            int count = 0;
+            for (int i = 0; i < GAME_SIZE; i++) {
+                if (gameField[i][j] == c) {
+                    count++;
+                    if (count == countToWin) return true;
+                } else {
+                    count = 0;
+                }
+            }
+        }
+        //Проверка диагонали на победу
+        int count = 0;
+        for (int i = 0; i < GAME_SIZE; i++) {
+            if (gameField[i][i] == c) {
+                count++;
+                if (count == countToWin) return true;
+            } else {
+                count = 0;
+            }
+        }
+
+        count = 0;
+        for (int i = 0; i < GAME_SIZE; i++) {
+            if (gameField[i][GAME_SIZE-1-i] == c) {
+                count++;
+                if (count == countToWin) return true;
+            } else {
+                count = 0;
+            }
+        }
         return false;
     }
 
