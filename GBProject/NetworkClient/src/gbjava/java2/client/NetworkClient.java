2d8e9242b86355946b20379f0330db662f4ec38b
dsoshnev <dsoshnev@mail.ru>
2020-03-21 14:55:26 +0300

    Выполнил задание по уроку Java 2.7 (предварительно)

---
+++ GBProject/NetworkClient/src/gbjava/java2/client/NetworkClient.java
@@ -0,0 +1,15 @@
+package gbjava.java2.client;
+
+import java.io.IOException;
+
+public class NetworkClient {
+
+    public static void main(String[] args) {
+        try {
+            ClientController clientController = new ClientController("localhost", 8189);
+            clientController.runApplication();
+        } catch (IOException e) {
+            System.err.println("Failed to connect to server! Please, check you network settings");
+        }
+    }
+}
